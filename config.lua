50e0a3a885cf511d96efccdcc6f6ccd80da4c8e6
Jakers <98612290+Jakerssss@users.noreply.github.com>
2022-08-03 15:14:06 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,7 @@
 Config = {}
 
+Config.Version = "new" -- Set to old for old qbcores
+
 Config["Command"] = true
 
-Config["Locations"] = {
-    ["mrpd"] = {
-        ["radius"] = 15,
-        ["coords"] = vector3(437.92, -1022.0, 28.45),
-    },
-}
-
 Config["OnlyPolice"] = false
-
-Config["Text"] = '<b>[E]</b> Open Menu' -- For cd_drawtextui
